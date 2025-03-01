11350b554f7e7a1cdda102ad23ec09e0768002f8
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2025-03-01 15:03:21 -0300

    chore: Add .gitignore

--- core/api.py
+++
@@ -1,5 +0,0 @@
-from ninja import NinjaAPI
-from treino.api import treino_router
-
-api = NinjaAPI()
-api.add_router('', treino_router)
--- treino/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TreinoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'treino'
